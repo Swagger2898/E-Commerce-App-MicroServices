130d0866fe52f1456bb042c4c771b09b3017981d
Swagger2898 <swapnilvrinda@gmail.com>
2024-12-30 17:35:46 +0530

    2nd commit

---
+++ services/customer/src/main/java/customer/CustomerService.java
@@ -0,0 +1,14 @@
+package customer;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class CustomerService {
+
+
+    public String createCustomer(@Valid CustomerRequest request) {
+
+
+
+    }
+}
